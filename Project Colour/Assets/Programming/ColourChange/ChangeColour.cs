265d71535509c290d7964d2b1629ba10b546e48d
AJ <1018212@student.sae.edu.au>
2021-02-22 13:57:21 +1100

    Colour changing works

---
+++ Project Colour/Assets/Programming/ColourChange/ChangeColour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AJ;
+
+namespace AJ
+{
+    public class ChangeColour : MonoBehaviour
+    {
+        private Color previousColor;
+        public Color currentColor;
+
+        
+        public void ChangeTo(Color newColor)
+        {
+            foreach (Renderer renderer in transform.GetComponentsInChildren<Renderer>())
+            {
+                Material mat = renderer.material;
+                previousColor           = mat.color;
+                renderer.material.color = newColor;
+                currentColor            = newColor;
+            }
+            ChangeTo(newColor);
+        }
+
+        public bool IsSameColour(Material a, Material b)
+        {
+            // TODO
+            return true;
+        }
+    }
+}
