57fcf137ccee17e1e8982d0a2076d1c6081bf9bd
PyrotechnicCake <pooooo12345>
2021-03-29 11:57:31 +1000

    Folder arrangement

---
+++ Project Colour/Assets/Scripts/Programming/FlockBehaviourScene/Behaviour Scripts/AvoidanceBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AJ
+{
+    [CreateAssetMenu(menuName = "Flock/Behaviour,Avoidance")]
+    public class AvoidanceBehaviour : FilteredFlockBehaviour
+    
+    {
+        private FlockBehaviour _flockBehaviourImplementation;
+
+        public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+        {
+            if (context.Count == 0)
+                return Vector3.zero;
+            
+            Vector3 avoidanceMove = Vector3.zero;
+            int nAvoid = 0;
+            List<Transform> filteredContext = (filter == null) ? context : filter.Filter(agent, context);
+            foreach (Transform item in filteredContext)
+            {
+                if (Vector3.SqrMagnitude(item.position - agent.transform.position) < flock.SquareAvoidanceRadius)
+                {
+                    nAvoid++;
+                    avoidanceMove += (Vector3)(agent.transform.position - item.position);
+                }
+            }
+            if (nAvoid > 0)
+                avoidanceMove /= nAvoid;
+            return avoidanceMove;
+        }
+    }
+}
