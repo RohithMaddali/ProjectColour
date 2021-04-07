9a9b323c77b2751ac2791f4fa2d9c274121b0108
Jordan Hanna <1015971@student.sae.edu.au>
2021-04-07 15:51:53 +1000

    Conveyer Belt
    Functionally for conveyer belts, future implementation will need third person controller to interact with it

---
+++ Project Colour/Assets/Scripts/ConveyerBelt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+    public GameObject belt;
+    public Transform endpoint;
+    public int speed;
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.transform.position, speed * Time.deltaTime);
+    }
+}
