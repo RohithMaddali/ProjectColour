8444cf266b0f852ea85b59348964930643df8783
Jordan Hanna <1015971@student.sae.edu.au>
2021-04-18 21:17:30 +1000

    Puzzle Mechanics
    Added some moving platforms and slide to represent waterfall, continue working on the project tomorrow

---
+++ Project Colour/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    public Vector3[] points; 
+    public int pointNumber = 0;
+    private Vector3 currentTarget;
+
+    public float tolerance;
+    public float speed;
+    public float delayTime;
+
+    private float delayStart;
+
+    public bool automatic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (points.Length > 0)
+        {
+            currentTarget = points[0];
+        }
+
+        tolerance = speed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != currentTarget)
+        {
+            MovePlatform();
+        }
+        else
+        {
+            UpdateTarget();
+        }
+    }
+
+    void MovePlatform()
+    {
+        Vector3 heading = currentTarget - transform.position;
+        transform.position += (heading / heading.magnitude) * (speed * Time.deltaTime);
+        if (heading.magnitude < tolerance)
+        {
+            transform.position = currentTarget;
+            delayStart = Time.time;
+        }
+    }
+    
+    void UpdateTarget()
+    {
+        if (automatic)
+        {
+            if (Time.time - delayStart > delayTime)
+            {
+                NextPlatform();
+            }
+        }
+    }
+
+    public void NextPlatform()
+    {
+        pointNumber++;
+        if (pointNumber >= points.Length)
+        {
+            pointNumber = 0;
+        }
+        currentTarget = points[pointNumber];
+    }
+    
+}
