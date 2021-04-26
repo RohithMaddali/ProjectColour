d4bcfe59bce27db4dfeb85c4f042d365f273c3b2
AJ <1018212@student.sae.edu.au>
2021-04-26 12:00:09 +1000

    Belt now works (with a timer to stop the belt)

--- Project Colour/Assets/Scripts/ConveyerBelt.cs
+++ Project Colour/Assets/Scripts/ConveyerBelt.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using AJ;
 using UnityEngine;
 
-public class ConveyerBelt : MonoBehaviour
+namespace Jordan
 {
-    public GameObject belt;
-    public Transform endpoint;
-    public int speed;
-    private void OnTriggerStay(Collider other)
+    public class ConveyerBelt : MonoBehaviour
     {
-        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.transform.position, speed * Time.deltaTime);
+        public GameObject belt;
+        public Transform endpoint;
+        bool isOn = false;
+        public float timer;
+        public int speed;
+        private void OnTriggerStay(Collider col)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                isOn = true;
+                Debug.Log("Belt is on");
+                StartCoroutine("BeltToggle");
+            }
+
+            if (isOn == true)
+            {
+                col.transform.position = Vector3.MoveTowards(col.transform.position, endpoint.transform.position, speed * Time.deltaTime);
+            }
+        }
+
+        IEnumerator BeltToggle()
+        {
+            yield return new WaitForSeconds(timer);
+            isOn = false;
+            Debug.Log("Belt is off");
+        }
     }
+
 }
