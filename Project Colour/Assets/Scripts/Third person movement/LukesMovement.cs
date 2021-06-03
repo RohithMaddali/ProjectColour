a200d90f5030e7d19382f8c1c5e158d56286f82c
Luke Stanley <1015748@student.sae.edu.au>
2021-06-03 18:16:46 +1000

    Walk and run speed based on joystick tilt

--- Project Colour/Assets/Scripts/Third person movement/LukesMovement.cs
+++ Project Colour/Assets/Scripts/Third person movement/LukesMovement.cs
@@ -18,7 +18,7 @@ public class LukesMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 2f;
-    public float maxSpeed = 15f;
+    public float maxSpeed = 10f;
     public float minSpeed = 2f;
     public float aimSpeed = 3f;
     public float acceleration = 2f;
@@ -68,18 +68,35 @@ public class LukesMovement : MonoBehaviour
     {
         if (moveCamActive)
         {
+            //check if grounded
             isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            //reset velocity when on ground so gravity stops increasing
             if (isGrounded && velocity.y < 0)
             {
                 velocity.y = -2f;
             }
+            //increase speed over time up to max speed 
             if (speed < maxSpeed)
             {
                 speed += acceleration * Time.deltaTime;
             }
-            
+            else if(speed > maxSpeed)
+            {
+                //reset down to new max speed if running then walking
+                speed = maxSpeed;
+            }
+            //get direction of movement
             Vector3 direction = new Vector3(move.x, 0f, move.y).normalized;
 
+            if(controls.Gameplay.Move.ReadValue<Vector2>().magnitude < .9f)
+            {
+                maxSpeed = 5f;
+            }
+            else if(controls.Gameplay.Move.ReadValue<Vector2>().magnitude >= .9f)
+            {
+                maxSpeed = 10f;
+            }
+            //move
             if (direction.magnitude >= 0.1f)
             {
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -88,21 +105,21 @@ public class LukesMovement : MonoBehaviour
 
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 controller.Move(moveDir.normalized * speed * Time.deltaTime);
-
-
             }
             else if (direction.magnitude < .1f)
             {
+                //reset speed when not moving
                 speed = 2f;
             }
         }
         else if (!moveCamActive)
         {
+            //aimCam movement
             Vector3 aimMove = transform.right * move.x + transform.forward * move.y;
 
             controller.Move(aimMove * aimSpeed * Time.deltaTime);
         }
-
+        //apply gravity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
@@ -114,7 +131,6 @@ public class LukesMovement : MonoBehaviour
             moveCam.Priority = 0;
             aimCam.Priority = 1;
             aimScript.aimCam = true;
-
         }
         else
         {
