1f5887b242ec546635e930c3423f875d44e34665
PyrotechnicCake <PyrotechnicCake@gmail.com>
2021-06-27 13:36:02 +1000

    Main menu quit option implemented

--- Project Colour/Assets/Scripts/MenuScript.cs
+++ Project Colour/Assets/Scripts/MenuScript.cs
@@ -6,10 +6,22 @@ namespace Pyro
 {
     public class MenuScript : MonoBehaviour
     {
+        public GameObject confirm;
+
         public void Quit()
         {
             Application.Quit();
-            Debug.Log ("Application has quit.");
+            Debug.Log("Application has quit.");
+        }
+
+        public void AskToQuit()
+        {
+            confirm.SetActive(true);
+        }
+
+        public void CancelQuit()
+        {
+            confirm.SetActive(false);
         }
     }
 }
