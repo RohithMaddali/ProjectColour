12dd036dba5620b1548942a4b588b25485f01148
PyrotechnicCake <PyrotechnicCake@gmail.com>
2021-06-27 13:18:21 +1000

    Additions to main menu

---
+++ Project Colour/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pyro
+{
+    public class MenuScript : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+            Debug.Log ("Application has quit.");
+        }
+    }
+}
