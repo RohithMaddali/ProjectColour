d1b9cf5ac6ad1bde80ae7e8c501dceb8642fdd88
KyanR <rowse9@gmail.com>
2021-07-02 15:23:03 +1000

    Volume Slider

---
+++ Project Colour/Assets/W_Ui.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W_Ui : MonoBehaviour
+{
+    public void VolumeSider(float volume)
+    {
+        AkSoundEngine.SetRTPCValue("Master_Volume", volume * 100);
+    }
+}
