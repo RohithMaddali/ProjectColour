6af8048838ef59e266cd8558d4f10aceb3bc9a0d
PyrotechnicCake <PyrotechnicCake@gmail.com>
2021-07-07 17:40:04 +1000

    Added rudimentary player movement animation
    (it's not good but don't worry will fix it later)

---
+++ Project Colour/Assets/Scripts/Third person movement/PlayerAnimator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pyro
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        public Animator animator;
+        RBMove rbmove;
+        PlayerControls controls;
+        float velocityX;
+        public float velocityZ;
+
+        public void Awake()
+        {
+            //animator = GetComponent<Animator>();
+            controls = new PlayerControls();
+            rbmove = GetComponentInParent<RBMove>();
+        }
+
+        void OnEnable()
+        {
+            controls.Gameplay.Enable();
+        }
+
+        void OnDisable()
+        {
+            controls.Gameplay.Disable();
+        }
+
+        public void Update()
+        {
+            velocityZ = controls.Gameplay.Move.ReadValue<Vector2>().magnitude;
+            animator.SetFloat("VelocityX", velocityX);
+            animator.SetFloat("VelocityZ", velocityZ);
+        }
+    }
+}
