fe89874530308d1dbbd2b078cfdc6330c5067da4
Rohith Maddali <1015083@student.sae.edu.au>
2021-07-16 09:50:17 +1000

    Added some UI and tweked minor changes in Red

---
+++ Project Colour/Assets/Scripts/TextDespawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDespawner : MonoBehaviour
+{
+    public GameObject tutInfo;
+    public int despawnTime;
+    void Start()
+    {
+        StartCoroutine(despawn());
+    }
+
+    IEnumerator despawn()
+    {
+        yield return new WaitForSeconds(despawnTime);
+        Destroy(tutInfo);
+    }
+}
