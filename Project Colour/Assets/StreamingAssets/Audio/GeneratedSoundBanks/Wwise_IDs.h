c8eb7eb534b78501ec187e5af6d1567aa4593dcc
KyanRowse <kyanrowsesounddesign@gmail.com>
2021-07-23 16:01:25 +1000

    Sound Improvements

--- Project Colour/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Project Colour/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,14 +14,18 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID EV_AMB_SWITCHER = 2110301309U;
+        static const AkUniqueID EV_BIRDS_WINGS_FLAPPING = 3702741518U;
+        static const AkUniqueID EV_BRIDGE_FIXED_01 = 761977599U;
         static const AkUniqueID EV_COLOUR_GRAB_SEQUENCE = 1156092748U;
         static const AkUniqueID EV_COLOUR_PLACE_SEQUENCE = 2911027737U;
         static const AkUniqueID EV_CONVEYER_BELT = 203475710U;
         static const AkUniqueID EV_IDLEVOX = 1133450584U;
         static const AkUniqueID EV_IMPACT_MOVE = 546473655U;
         static const AkUniqueID EV_MACHINERY_MOVE = 3244770519U;
+        static const AkUniqueID EV_MUSHROOM = 1125063709U;
         static const AkUniqueID EV_MX_EXPLORE = 2266659196U;
         static const AkUniqueID EV_SCREACHING_METAL = 794110178U;
+        static const AkUniqueID EV_SEAGULLS = 3893023783U;
         static const AkUniqueID EV_SERVO = 878682118U;
         static const AkUniqueID EV_SFX_BIRDS = 1994831277U;
         static const AkUniqueID EV_SFX_PLR_FOOSTEPS = 1061346095U;
