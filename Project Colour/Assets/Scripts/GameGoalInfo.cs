a7573fc4ab0e45cc19419d3d835944e4cf58303c
KyanRowse <kyanrowsesounddesign@gmail.com>
2021-08-04 21:26:33 +1000

    Main Menu Music Setup

--- Project Colour/Assets/Scripts/GameGoalInfo.cs
+++ Project Colour/Assets/Scripts/GameGoalInfo.cs
@@ -9,26 +9,28 @@ public class GameGoalInfo : MonoBehaviour
     public Scene thisScene;
     public bool infoGiven = false;
     public int delayTimer;
+    W_MainMenuMusic mainMenuMusic;
 
     void Start()
     {
+        mainMenuMusic = GameObject.Find("music").GetComponent<W_MainMenuMusic>();
         StartCoroutine(info());
-    }
-
-    void Update()
-    {
-
     }
     IEnumerator info()
     {
         yield return new WaitForSeconds(delayTimer);
+        mainMenuMusic.StartCoroutine(mainMenuMusic.FadeMusic());
         anim.SetBool("infoSpawn", true);
         yield return new WaitForSeconds(18f);
         loadScene();
     }
-
     public void loadScene()
     {
         SceneManager.LoadScene("AdditiveSceneTest", LoadSceneMode.Single);
+        mainMenuMusic.StopMusic();
+    }
+    public void StopMusic()
+    {
+        mainMenuMusic.StopMusic();
     }
 }
---
+++ Project Colour/Assets/W_MainMenuMusic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W_MainMenuMusic : MonoBehaviour
+{
+    uint mainMenuMusic;
+    uint voiceOver;
+
+    private void Start()
+    {
+        voiceOver = AkSoundEngine.PostEvent("ev_waking_the_weapon", gameObject);
+    }
+    public void StartMainMenuMusic()
+    {
+        mainMenuMusic = AkSoundEngine.PostEvent("ev_mx_mainmenu_and_intro_music", gameObject);
+        AkSoundEngine.StopPlayingID(voiceOver);
+    }
+    public void StartGame()
+    {
+       AkSoundEngine.SetRTPCValue("track_volume", 100);
+    }
+    public void StopMusic()
+    {
+        AkSoundEngine.StopPlayingID(mainMenuMusic);
+    }
+    public IEnumerator FadeMusic()
+    {
+        yield return new WaitForSeconds(10);
+        AkSoundEngine.SetRTPCValue("main_menu_music_volume", 0);
+    }
+    
+}
