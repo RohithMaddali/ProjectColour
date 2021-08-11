b7663d2bf8fdaec3beaa4c85c81fb80e6b7a697b
KyanRowse <kyanrowsesounddesign@gmail.com>
2021-08-11 20:49:50 +1000

    UI Sounds and Jumppad Sound

--- Project Colour/Assets/W_Ui.cs
+++ Project Colour/Assets/W_Ui.cs
@@ -6,6 +6,27 @@ public class W_Ui : MonoBehaviour
 {
     public void VolumeSider(float volume)
     {
-        AkSoundEngine.SetRTPCValue("Master_Volume", volume * 100);
+        AkSoundEngine.SetRTPCValue("Master_Volume", volume);
+    }
+    public void SFXBus(float volume)
+    {
+        AkSoundEngine.SetRTPCValue("sfx_bus_volume", volume);
+    }
+    public void MusicBus(float volume)
+    {
+        AkSoundEngine.SetRTPCValue("music_bus_volume", volume);
+    }
+    public void VoiceOverBus(float volume)
+    {
+        AkSoundEngine.SetRTPCValue("voice_over_bus_volume", volume);
+    }
+    public void OnClick()
+    {
+        AkSoundEngine.PostEvent("ev_sfx_ui_hover", gameObject);
+    }
+    public void OnHover()
+    {
+        AkSoundEngine.PostEvent("ev_sfx_ui_click", gameObject);
     }
 }
+
