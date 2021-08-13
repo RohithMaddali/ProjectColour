7b0af4fd3ab7ff2b753f24461c12beae265b259c
KyanRowse <kyanrowsesounddesign@gmail.com>
2021-08-13 21:44:16 +1000

    Endgame Music

--- Project Colour/Assets/Scripts/Wwise/W_Music.cs
+++ Project Colour/Assets/Scripts/Wwise/W_Music.cs
@@ -6,44 +6,63 @@ using UnityEngine;
 public class W_Music : MonoBehaviour
 {
     private uint musicExploreID;
+    GameManager gm;
     void Start()
     {
-        AkSoundEngine.SetSwitch("MusicSwitch", "Hub", gameObject);
+        gm = FindObjectOfType<GameManager>();
+        if (gm.itemsPicked == 6)
+        {
+            AkSoundEngine.SetSwitch("MusicSwitch", "EndGame", gameObject);
+        }
+        else
+        {
+            AkSoundEngine.SetSwitch("MusicSwitch", "Hub", gameObject);
+        }    
+
         musicExploreID = AkSoundEngine.PostEvent("ev_mx_explore", gameObject);
     }
-
     private void OnDestroy()
     {
         AkSoundEngine.StopPlayingID(musicExploreID);
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("GreenTriger"))
+        if (gm.itemsPicked != 6)
         {
-            Debug.Log("green trigger");
-            AkSoundEngine.SetSwitch("MusicSwitch", "Green", gameObject);
-            AkSoundEngine.SetState("Colour", "Green");
-        }
-        if (other.gameObject.CompareTag("BlueTrigger"))
-        {
-            Debug.Log("blue trigger");
-            AkSoundEngine.SetSwitch("MusicSwitch", "Blue", gameObject);
-            AkSoundEngine.SetState("Colour", "Blue");
-        }
-        if (other.gameObject.CompareTag("RedTrigger"))
-        {
-            Debug.Log("green trigger");
-            AkSoundEngine.SetSwitch("MusicSwitch", "Red", gameObject);
-            AkSoundEngine.SetState("Colour", "Red");
+            if (other.gameObject.CompareTag("GreenTriger"))
+            {
+                Debug.Log("green trigger");
+                AkSoundEngine.SetSwitch("MusicSwitch", "Green", gameObject);
+                AkSoundEngine.SetState("Colour", "Green");
+            }
+            if (other.gameObject.CompareTag("BlueTrigger"))
+            {
+                Debug.Log("blue trigger");
+                AkSoundEngine.SetSwitch("MusicSwitch", "Blue", gameObject);
+                AkSoundEngine.SetState("Colour", "Blue");
+            }
+            if (other.gameObject.CompareTag("RedTrigger"))
+            {
+                Debug.Log("green trigger");
+                AkSoundEngine.SetSwitch("MusicSwitch", "Red", gameObject);
+                AkSoundEngine.SetState("Colour", "Red");
+            }
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("GreenTriger") || other.gameObject.CompareTag("BlueTrigger") || other.gameObject.CompareTag("RedTrigger"))
         {
-            Debug.Log("exit green");
-            AkSoundEngine.SetState("Colour", "None");
-            AkSoundEngine.SetSwitch("MusicSwitch", "hub", gameObject);
+            if (gm.itemsPicked == 6)
+            {
+                AkSoundEngine.SetState("Colour", "None");
+                AkSoundEngine.SetSwitch("MusicSwitch", "EndGame", gameObject);
+            }
+            else
+            {
+                AkSoundEngine.SetState("Colour", "None");
+                AkSoundEngine.SetSwitch("MusicSwitch", "hub", gameObject);
+            }
         }
     }
 }
